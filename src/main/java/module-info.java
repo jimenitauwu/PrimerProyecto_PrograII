98b4ddfda1e5cd0594e8dad2ffa6357487dea365
Jimena <jimenaguevarach3@gmail.com>
2024-05-18 23:51:07 -0600

    Primer Proyecto Progra II

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ucr.ac.cr.paraiso.xml.primerproyecto_programacionii {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ucr.ac.cr.paraiso.xml.primerproyecto_programacionii to javafx.fxml;
+    exports ucr.ac.cr.paraiso.xml.primerproyecto_programacionii;
+}
---
+++ src/main/java/ucr/ac/cr/paraiso/xml/primerproyecto_programacionii/HelloController.java
@@ -0,0 +1,14 @@
+package ucr.ac.cr.paraiso.xml.primerproyecto_programacionii;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
