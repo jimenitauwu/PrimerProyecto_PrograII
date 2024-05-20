de0de02eae7a4131da37679c6d3e40c5eac8868f
Jimena <jimenaguevarach3@gmail.com>
2024-05-20 12:36:19 -0600

    interfaces borrar y buscar

---
+++ src/main/java/ucr/ac/cr/paraiso/primerproyecto_programacionII/controller/BorrarPatronController.java
@@ -0,0 +1,24 @@
+package ucr.ac.cr.paraiso.primerproyecto_programacionII.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class BorrarPatronController
+{
+    @javafx.fxml.FXML
+    private Button btn_Borrar;
+    @javafx.fxml.FXML
+    private Label message_Eliminado;
+    @javafx.fxml.FXML
+    private TextField textField_Eliminar;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void EliminarOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/ucr/ac/cr/paraiso/primerproyecto_programacionII/controller/BuscarPatronController.java
@@ -0,0 +1,24 @@
+package ucr.ac.cr.paraiso.primerproyecto_programacionII.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+public class BuscarPatronController
+{
+    @javafx.fxml.FXML
+    private Button btn_Buscar;
+    @javafx.fxml.FXML
+    private TextField textField_Buscar;
+    @javafx.fxml.FXML
+    private BorderPane borderPane;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void BuscarOnAction(ActionEvent actionEvent) {
+    }
+}
